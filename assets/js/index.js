789671545035e6a943a410e737e3824ae0aea6b1
Marco Isaías <marcoramirez3700@gmail.com>
2023-06-18 02:16:59 -0600

    scroll reveal
    scroll reveal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,46 @@
+/* ----------- CONST AN VARIABLES ----------- */
+var themeSelect = localStorage.getItem("theme");
+const theme = document.getElementById('toogle-theme')
+const bars = document.getElementById('bars')
+const navSecond = document.getElementById('nav-second')
+const navFirst = document.getElementById('nav-first')
+const elements = document.querySelectorAll('.swiper-slide');
+
+/* --------------- LOAD THEME ------------------*/
+if (themeSelect !== null || themeSelect === 'light') {
+    if (themeSelect === 'dark') {
+        document.body.classList.add('dark-theme')
+        theme.classList.remove('fa-moon')
+        theme.classList.add('fa-sun')
+    }
+}
+
+/* ------------- TOGGLE SIDE BAR ----------------*/
+bars.onclick = function () {
+    navSecond.classList.toggle('open')
+    navFirst.classList.toggle('open-1')
+}
+
+/*-------------- CHANGE THEME -------------------*/
+theme.onclick = function () {
+    if (themeSelect !== 'dark') {
+        document.body.classList.add('dark-theme')
+        localStorage.setItem("theme", "dark");
+        themeSelect = 'dark'
+        theme.classList.remove('fa-moon')
+        theme.classList.add('fa-sun')
+    } else {
+        document.body.classList.remove('dark-theme')
+        localStorage.setItem("theme", "light");
+        themeSelect = 'light'
+        theme.classList.remove('fa-sun')
+        theme.classList.add('fa-moon')
+    }
+}
+
+/* ------------------- SWIPER -----------------*/
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
-
     spaceBetween: 30,
     direction: "vertical",
     pagination: {
@@ -9,24 +49,19 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-const elementos = document.querySelectorAll('.swiper-slide');
-
-// Agrega un evento mouseenter a cada elemento
-elementos.forEach(elemento => {
-    elemento.addEventListener('mouseenter', () => {
-        // Recorre todos los elementos y cambia el color de fondo
-        elementos.forEach(el => {
-            if (el !== elemento) {
+/* ---------- EFECT BLUR ELEMENTS FROM SWIPER ---------*/
+elements.forEach(element => {
+    element.addEventListener('mouseenter', () => {
+        elements.forEach(el => {
+            if (el !== element) {
                 el.classList.remove("select")
                 el.classList.add("not-select")
             }
-
         });
     });
 
-
-    elemento.addEventListener('mouseleave', () => {
-        elementos.forEach(el => {
+    element.addEventListener('mouseleave', () => {
+        elements.forEach(el => {
             el.classList.remove("not-select")
             el.classList.add("select")
         });
@@ -34,11 +69,17 @@ elementos.forEach(elemento => {
 });
 
 
-const bars = document.getElementById('bars')
-const navSecond = document.getElementById('nav-second')
-const navFirst = document.getElementById('nav-first')
+/*------------------ SCROLL REVEAL ANIMATION ---------------*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
 
-bars.onclick = function(){
-    navSecond.classList.toggle('open')
-    navFirst.classList.toggle('open-1')
-}
+sr.reveal(`#home`)
+sr.reveal(`#shop`, {delay: 500})
+sr.reveal(`#pages`, {delay: 600})
+sr.reveal(`#bloc`,{origin: 'left'})
+sr.reveal(`#contact`,{origin: 'right'})
+sr.reveal(`#footer`,{interval: 100})
